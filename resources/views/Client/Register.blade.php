e7b0e159f9820c793c7dd30937237803eeecf920
Daniel-Salazar-A <danysalazar430@gmail.com>
2026-05-04 13:49:10 -0600

    Add layouts and yield

--- resources/views/Client/Register.blade.php
+++ resources/views/Client/Register.blade.php
@@ -1,63 +1,5 @@
-<!-- resources/views/tienda.blade.php -->
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Tienda Online</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        .navbar-brand {
-            font-weight: bold;
-        }
-        .card-img-top {
-            height: 200px;
-            object-fit: cover;
-        }
-        footer {
-            background-color: #212529;
-            color: white;
-            padding: 20px 0;
-            margin-top: 40px;
-        }
-    </style>
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">Mi Tienda</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('mainClient') }}">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Carrito</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contacto</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#{{ route('register') }}">Registro</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+@extends('layouts.app')
+@section('content')
 
 <div class="container mt-5">
     <div class="row justify-content-center">
@@ -82,6 +24,11 @@
                         </div>
 
                         <div class="mb-3">
+                            <label class="form-label">Número de teléfono</label>
+                            <input type="tel" name="telefono" class="form-control" required>
+                        </div>
+
+                        <div class="mb-3">
                             <label class="form-label">Contraseña</label>
                             <input type="password" name="password" class="form-control" required>
                         </div>
@@ -101,5 +48,4 @@
     </div>
 </div>
 
-</body>
-</html>
+@endsection
